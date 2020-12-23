d2af20f8a7fbc50d1202c231e404cfa14b09cca1
Julian Beck <ju-fa-beck@t-online.de>
2020-12-23 09:57:00 +0100

    implemented q_learning

--- aufgabe6/frozen_q-learning.py
+++ aufgabe6/frozen_q-learning.py
@@ -6,10 +6,57 @@ env = gym.make("FrozenLake-v0")
 random.seed(0)
 np.random.seed(0)
 env.seed(0)
+lr = 0.1
+gamma = 0.9
 
 print("## Frozen Lake ##")
 print("Start state:")
 env.render()
+#Q-Learning
+#“If one had to identify one idea as central and novel to reinforcement learning,
+#it would undoubtedly be temporal differencelearning.“ - Sutton Book 
+#• Temporal differencing (TD) learning is a combination of dynamic programming (DP) and Monte Carlo (MC) methods.
+#• Like MC methods, TD methods can learn directly from experience without knowing the MDP structure.
+#• Like DP, TD methods update estimates based partially on other learned estimates,
+#  without waiting for a final outcome (“bootstrapping”).
+# Use reward and estimated state value of next state to update current state value
+#TD: New evidence based on current reward and state value of next state
+#Q-learning is off-policy and uses:
+#• TD-prediction for policy evaluation.
+#• An -greedy policy as behaviour policy.
+#• A greedy policy as target policy.
+#• One can show that the learned Q-values directly approximate the optimal Q-values.
+
+
+def play_episode(q_values, epsilon):
+    state = env.reset()
+    done = False
+    r_s = []
+    while not done:
+        #choose action action
+        action = choose_action(q_values, state, epsilon)
+        #take action
+        state2, reward, done, _ = env.step(action)
+        #update Q(S,A)+learningRate(R+gamma maxQ(S´, a) - Q(S,A)))
+        updateQ(state, state2, reward, action, q_values)
+        state = state2
+        r_s.append(reward)
+    return r_s
+
+def updateQ(state, state2, reward, action, q_values):
+    predict = q_values[state, action]
+    target = reward + gamma * np.max(q_values[state2, :])
+    q_values[state, action] = q_values[state, action] + lr * (target - predict)
+
+def init_q():
+    return np.zeros((env.observation_space.n, env.action_space.n))
+
+
+def choose_action(q_values, state, epsilon):
+    if random.random() > epsilon:
+        return np.argmax(q_values[state, :])
+    else:
+        return env.action_space.sample()
 
 def main():
     no_episodes = 1000
@@ -20,13 +67,8 @@ def main():
         rewards = []
         q_values = init_q()
         for i in range(0, no_episodes):
-            s, r = play_episode(q_values, epsilon=e)
+            r = play_episode(q_values, epsilon=e)
             rewards.append(sum(r))
-
-            # update q-values
-            for i2, q in enumerate(s):
-                return_i = sum(r[i2:])
-                q_values[q] += 0.3 * (return_i - q_values[q])
 
         plot_data.append(np.cumsum(rewards))
 
--- aufgabe6/mc_control.py
+++ aufgabe6/mc_control.py
@@ -47,6 +47,25 @@ def choose_action(q_values, state, epsilon):
 
 
 def main():
+    #spielt Frozen lake mit verschiedenen Epsilons (Explorieren)
+    #Monte Carlo Control
+    #Nach jeder Episode updaten der Q-Values
+    #Summe des Rewards
+    #Monte Carlo control methods do not suffer from this bias, 
+    # as each update is made using a true sample of what Q(s,a) should be. 
+    # However, Monte Carlo methods can suffer from high variance, 
+    # which means more samples are required to achieve the same degree of learning compared to TD.
+    #Monte Carlo Prediction (MC)
+    #Any method which solves a problem by generating suitable random numbers,
+    #and observing that fraction of numbers obeying some property or properties,
+    #can be classified as a Monte Carlo method.”1
+    #“Given a policy, create sample episodes following that policy
+    # and estimate state and Q-values from the collected experiences.”
+    #No knowledge about MDP needed (model-free approach).
+    # The more episodes we sample, the closer the estimates of the state
+    # values approach the real state values (law of large numbers).
+    # Drawback: Can only be applied to episodic problems.
+    #MC: New evidence based on observed return (at end of episode)
     no_episodes = 1000
     epsilons = [0.01, 0.1, 0.5, 1.0]
 
@@ -55,9 +74,11 @@ def main():
         rewards = []
         q_values = init_q()
         for i in range(0, no_episodes):
+            #Sample episodes: Start from start state and follow policy π until terminal state n is reached. Repeat this times.
             s, r = play_episode(q_values, epsilon=e)
+            #Calculate empirical return Gt in every state that was visited.
             rewards.append(sum(r))
-
+            #Calculate V(s) for every state by aggregating the empirical returns received in s.
             # update q-values
             for i2, q in enumerate(s):
                 return_i = sum(r[i2:])
@@ -72,6 +93,7 @@ def main():
         plt.plot(range(0, no_episodes), plot_data[i], label="e=" + str(eps))
     plt.legend()
     plt.show()
+    #Plot der Summe von Rewards
 
 
 main()
