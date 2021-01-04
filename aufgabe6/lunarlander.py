ab5e8f44ddb924d2988fcb2776608b8367f51d03
Julian Beck <ju-fa-beck@t-online.de>
2021-01-04 15:05:37 +0100

    mean reward

--- aufgabe6/lunarlander.py
+++ aufgabe6/lunarlander.py
@@ -1,13 +1,15 @@
 import gym
+import numpy as np
 env = gym.make('LunarLander-v2')
-for i_episode in range(20000):
+rewards = []
+for i_episode in range(2000):
     observation = env.reset()
     for t in range(100):
-        env.render()
-        print(observation)
+        #env.render()
         action = env.action_space.sample()
         observation, reward, done, info = env.step(action)
+        rewards.append(reward)
         if done:
-            print("Episode finished after {} timesteps".format(t+1))
+            print(f"Max Reward {np.max(np.array(rewards))} timesteps")
             break
 env.close()
